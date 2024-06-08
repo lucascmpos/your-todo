f122fbc9051670d3d158f3b7298c3f732f11e995
Lucas Campos <lucasoliveiracampos81@gmail.com>
2024-06-08 19:52:53 -0300

    fixing

--- src/app/app/settings/billing/actions.ts
+++ src/app/app/settings/billing/actions.ts
@@ -17,7 +17,7 @@ export async function createCheckoutSessionAction() {
   const checkoutSession = await createCheckoutSession(
     session.user.id as string,
     session.user.email as string,
-    session.user.stripeSubscriptionId as string,
+    (session.user as any).stripeSubscriptionId as string,
   )
 
   if (!checkoutSession.url) return
