7844478f43ff04992902546b0ad40cbc5cd2c32b
Lucas Campos <lucasoliveiracampos81@gmail.com>
2024-06-08 19:44:47 -0300

    fix

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,13 +5,17 @@ export function middleware(request: NextRequest) {
   const token = request.cookies.get('authjs.session-token')
   const pathname = request.nextUrl.pathname
 
+  console.log('Middleware:', { pathname, token }) // Adicionar log para depuração
+
   if (pathname === '/auth' && token) {
-    return NextResponse.redirect(new URL(getUrl('/app')))
+    return NextResponse.redirect(new URL(getUrl('/app'), request.url))
   }
 
   if (pathname.includes('/app') && !token) {
-    return NextResponse.redirect(new URL(getUrl('/auth')))
+    return NextResponse.redirect(new URL(getUrl('/auth'), request.url))
   }
+
+  return NextResponse.next() // Garantir que outras requisições prossigam normalmente
 }
 
 export const config = {
